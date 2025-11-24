ba15499d7b5ba5f65bf57baa71dce53c096e1afa
Shay. <shay@shaybox.local>
2025-11-23 22:08:59 -0500

    Configure DMARC/SPF for email domains

--- modules/ses-verified-domain/variables.tf
+++ modules/ses-verified-domain/variables.tf
@@ -4,3 +4,13 @@ variable "domain" {
 variable "zone_id" {
   type = string
 }
+
+variable "dmarc" {
+  type = object({
+    # policy on invalid DKIM headers - 'reject' blocks delivery
+    p = optional(string, "quarantine")
+    # subdomain policy - "none" allows email from arbitrary subdomains
+    sp = optional(string, "none")
+  })
+  default = {}
+}
